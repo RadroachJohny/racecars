777b88d154a3b7ec8ec3340340eceec76d9ecd4a
Igor Lait <radroachjohny@gmail.com>
2020-09-10 20:32:54 +0300

    final lesson

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const score = document.querySelector('.score'),
       start = document.querySelector('.start'),
       gameArea = document.querySelector('.gameArea'),
-      car = document.createElement('div');
+      car = document.createElement('div'),
+      heightElem = 100;
 
 car.classList.add('car');
+
+const countSection = Math.floor(document.documentElement.clientHeight / heightElem);
+
+gameArea.style.height = countSection * heightElem;
+
+
 
 start.addEventListener('click', startGame);
 
@@ -20,37 +27,43 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 2,
+  speed: 3,
   traffic: 3
 };
 
 function getQuantityElements(heightElement) {
-  return document.documentElement.clientHeight / heightElement + 1;
+  return (gameArea.offsetHeight / heightElement) + 1;
 };
 
 function startGame(){
   start.classList.add('hide');
+  gameArea.innerHTML = '';
+  
 
-  for (let i = 0; i < getQuantityElements(100); i++) {
+  for (let i = 0; i < getQuantityElements(heightElem); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
-    line.style.top = (i * 100) + 'px';
-    line.y = i * 100;
+    line.style.top = (i * heightElem) + 'px';
+    line.y = i * heightElem;
     gameArea.appendChild(line);
   };
 
-  for ( let i = 0; i < getQuantityElements(100 * setting.traffic); i++){
+  for ( let i = 0; i < getQuantityElements(heightElem * setting.traffic); i++){
     const enemy = document.createElement('div');
     enemy.classList.add('enemy');
-    enemy.y = -100 * setting.traffic * (i + 1);
-    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.y = -heightElem * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - heightElem/2)) + 'px';
     enemy.style.top = enemy.y + 'px';
-    enemy.style.background = "url('../image/redenemy.png') center / cover no-repeat";
+    enemy.style.background = `url('../image/enemy${Math.floor(Math.random() * 3)}.png') center / cover no-repeat`;
     gameArea.appendChild(enemy);
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  car.style.bottom = '10px';
+  car.style.top = 'auto';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -58,6 +71,8 @@ function startGame(){
 
 function playGame() {
   if (setting.start === true) {
+    setting.score += setting.speed;
+    score.innerHTML = 'score<br> ' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -98,8 +113,8 @@ function moveRoad() {
   lines.forEach( function(line) {
     line.y += setting.speed;
     line.style.top = line.y +'px';
-    if (line.y >= document.documentElement.clientHeight) {
-      line.y = -100;
+    if (line.y >= gameArea.offsetHeight) {
+      line.y = -heightElem;
     }
   });
 }
@@ -107,14 +122,27 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left + 5 <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top) {
+        console.warn('collision!');
+        setting.start = false;
+        start.classList.remove('hide');
+        start.style.top = score.offsetHeight;
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
-    if (item.y >= document.documentElement.clientHeight) {
-      item.y = -100 * setting.traffic;
-      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    if (item.y >= gameArea.offsetHeight) {
+      item.y = -heightElem * setting.traffic;
+      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - heightElem/2)) + 'px';
+      item.style.background = `url('../image/enemy${Math.floor(Math.random() * 3)}.png') center / cover no-repeat`;
     }
   });
-
   
 };
 
